13568d796005c1dcf79c1b8a2dda3d2eca692b12
Abhinash Shrestha <mr.indestructible010@gmail.com>
2023-12-21 13:18:20 +0545

    Delete some models cuz they were not good enough

--- Client_Programs/gesture_efficientnet.py
+++ Client_Programs/gesture_efficientnet.py
@@ -14,7 +14,7 @@ import time
 # Initialize a variable to hold the time of the last detection
 last_detection_time = None
 
-skip_frames = 10
+skip_frames = 1
 # Load class_names
 #Keras’s image_dataset_from_directory function generates labels as integer indices that 
 # correspond to the alphabetical order of the class names. 
@@ -25,7 +25,7 @@ class_names = ["Brightness_Decrease", "Brightness_Increase", "Chrome_Open", "Cur
     
 # print(class_names)
 # Load the trained model for efficientnet
-model = load_model('../Models/V2M_alpha.h5')
+model = load_model('../Models/V2M.h5')
 
 
 # Initialize MediaPipe Hands
@@ -67,6 +67,10 @@ while cap.isOpened():
         # Draw the hand annotations on the image
         if results.multi_hand_landmarks:
             for hand_landmarks in results.multi_hand_landmarks:
+                #the below will draw landmarks but if that is given to the model then the model will predict all classes to be nothing
+                # mp_drawing.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+                
+                
                 # Draw bounding box around the hand with some padding
                 hand_landmarks_array = np.array([[data.x, data.y, data.z] for data in hand_landmarks.landmark])
                 x_min, y_min, z_min = np.min(hand_landmarks_array, axis=0)
@@ -84,7 +88,9 @@ while cap.isOpened():
 
 
                 # Extract the hand image
+                
                 hand_img = frame[bbox[1]:bbox[3], bbox[0]:bbox[2]]
+                
                 # Preprocess the hand image
                 img = cv2.resize(hand_img, (img_height, img_width))
                 img = image.img_to_array(img)
@@ -122,27 +128,27 @@ while cap.isOpened():
                 predictions = model.predict(img) 
                 predicted_class = np.argmax(predictions[0])
                 confidence = np.max(predictions[0])
-                handler = ActionHandler(class_names[predicted_class])
-                handler.execute_action()
+                # handler = ActionHandler(class_names[predicted_class])
+                # handler.execute_action()
 
                 # Print the class name and confidence
                 print('The predicted class is:', class_names[predicted_class])
                 print('Confidence:', confidence)
 
-    # Draw the bounding box
+    # # Draw the bounding box
     if bbox is not None:
-        cv2.rectangle(frame, (bbox[0], bbox[1]), (bbox[2], bbox[3]), (0, 255, 0), 4)  # Change the last parameter to adjust the thickness of the bounding box
+        cv2.rectangle(frame, (bbox[0], bbox[1]), (bbox[2], bbox[3]), (255, 255, 0), 3)  # Change the last parameter to adjust the thickness of the bounding box
 
     # Display the class name above the bounding box
     if predicted_class is not None:
-        cv2.putText(frame, class_names[predicted_class], (bbox[0], bbox[1]-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36,255,12), 2)
-
+        cv2.putText(frame, class_names[predicted_class],(bbox[0], bbox[1]-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0,255,255), thickness=2,lineType=cv2.LINE_AA)    
+    
     # Increment the frame counter
     frame_counter += 1
 
     # Display the resulting frame
     cv2.imshow('MediaPipe Hands', frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if cv2.waitKey(1) & 0xFF == 27:
         break
 
 cap.release()
